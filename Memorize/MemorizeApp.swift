dd8dda9fe20cb83746a5d4674ba8abd08b51dbc5
Ayan Sarkar <sarkar_ayan@icloud.com>
2023-08-16 23:36:01 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ayan Sarkar on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
